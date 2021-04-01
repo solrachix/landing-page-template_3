8328bb5914f0f64faed1bf72ed9dbe4735f0a006
Carlos Miguel <carlos.miguel.oliveira.17@gmail.com>
2021-04-01 09:34:11 -0300

    adding: lottie animation

--- src/components/PageTransition/index.tsx
+++ src/components/PageTransition/index.tsx
@@ -1,15 +1,25 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
+
 import styled from 'styled-components'
-import { useTransition, animated, config } from 'react-spring'
+import Lottie from 'react-lottie'
+import { useTransition, animated, config, useSpring } from 'react-spring'
+
+import animation from '@/assets/animations/intro.json'
 import { useRouter } from '../../hooks/useRouter'
 
 export const PageTransition = ({ children, ...props }) => {
   const router = useRouter()
+  const [finishing, setFinishing] = useState(false)
+  const [spring, set] = useSpring<{ opacity: number; scale: number }>(() => ({
+    opacity: 0,
+    scale: 0,
+    config: { mass: 10, tension: 200, friction: 40 }
+  }))
 
   const transitions = useTransition(router, router => router.pathname, {
-    from: { transform: 'translate3d(100vw, 0, 0)' },
-    enter: { opacity: 1, transform: 'translate3d(0, 0, 0)' },
-    leave: { opacity: 0, transform: 'translate3d(-100vw, 0, 0)' },
+    from: { opacity: 0 },
+    enter: { opacity: 1 },
+    leave: { opacity: 0 },
     config: config.wobbly
 
     // from: { transform: 'translateY(-2000px)' },
@@ -17,7 +27,20 @@ export const PageTransition = ({ children, ...props }) => {
     // leave: { transform: 'translateY(200px)' },
     // config: config.stiff
   })
+  const defaultAnimationOptions = {
+    loop: false,
+    autoplay: true,
+    animationData: animation,
+    rendererSettings: {
+      preserveAspectRatio: 'xMidYMid slice'
+    }
+  }
 
+  useEffect(() => {
+    if (finishing) {
+      set({ opacity: 1, scale: 1 })
+    }
+  }, [finishing])
   return (
     <>
       {transitions.map(({ item, props: style, key }) => {
@@ -28,10 +51,33 @@ export const PageTransition = ({ children, ...props }) => {
         const { Component, props } = item.components[item.pathname]
 
         return (
-          <Page key={key} style={style}>
-            {children(
-              item ? { Component, pageProps: props && props.pageProps } : {}
+          <Page style={style} key={key}>
+            {!finishing && (
+              <LottieContainer>
+                <Lottie
+                  options={defaultAnimationOptions}
+                  eventListeners={[
+                    {
+                      eventName: 'complete',
+                      callback: () => setFinishing(true)
+                    }
+                  ]}
+                  width="50%"
+                  height="auto"
+                />
+              </LottieContainer>
             )}
+
+            <animated.div
+              style={{
+                opacity: spring.opacity,
+                transform: `scale(${spring.scale})`
+              }}
+            >
+              {children(
+                item ? { Component, pageProps: props && props.pageProps } : {}
+              )}
+            </animated.div>
           </Page>
         )
       })}
@@ -39,7 +85,22 @@ export const PageTransition = ({ children, ...props }) => {
   )
 }
 
-const Page = styled(animated.main)`
+const Page = styled(animated.div)`
   min-height: 100%;
   height: 100%;
+`
+const LottieContainer = styled(animated.div)`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100vw;
+  height: 100vh;
+
+  background: #04d361;
+
+  z-index: 10;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
 `
